65127156a7b3ae5a703c2579a2d5ad5b84d7df39
Charles Che <chang.che1106@gmail.com>
2018-04-10 21:09:35 -0400

    5 states comparison
    change to the Comparison to states of 5

---
+++ BaumWelch.R
@@ -0,0 +1,33 @@
+BaumWelch_hmm <- function(obs.vec, pi.mar, tran.m, mean.vec, sd.vec, maxiter, epsilon){
+  oldLL <- -Inf
+  log.lik.iter <- c(-Inf)
+  for (iter in 1:maxiter){
+    FB <- ForwardBack2(obs.vec, pi.mar, tran.m, mean.vec, sd.vec)
+    LL <- FB$Loglikelihood
+    log.lik.iter <- c(log.lik.iter, LL)
+    diff <- LL - oldLL
+    cat('iter=', iter, '\n')
+    cat('LL=', round(LL, 10), '\n')
+    cat('diff=', diff, '\n')
+    if(abs(diff)<epsilon){
+      stop('Algorithm converges!')
+    }
+    #can add more criterions when needed
+    gamma.log <- FB$alphalog+FB$betalog-LL
+    gamma.m <- exp(gamma.log)
+    AH <- FB$alphahat
+    BH <- FB$betahat
+    DM <- FB$density_m
+    ct_vec <-FB$ct
+    gamma.m <- AH*BH
+    gamma.m <- t(sapply(1:Ti, function(x) gamma.m[x,]/ct_vec[x]))
+    mean.vec <- apply(gamma.m, 2, function(x) sum(x*obs.vec)/sum(x))
+    sd.vec <- sqrt(apply((matrix(obs.vec, nrow = Ti, ncol = N) - matrix(mean.vec,nrow = Ti, ncol = N, byrow = TRUE))^2 * gamma.m, MARGIN = 2,FUN = sum)/apply(gamma.m, MARGIN = 2, FUN = sum))
+    pi.mar <- gamma.m[1,]
+    tran.m <- (t(AH[1:Ti-1,])%*%(DM[2:Ti,]*BH[2:Ti,]))*tran.m #maybe a parenthesis is redundant.
+    #tran.m <- t(scale(t(tran.m),center = F, scale = colSums(t(tran.m))))
+    tran.m <- t(apply(tran.m, 1, function(x) x/sum(x)))
+    oldLL <- LL
+  }
+  return(list(tran_m = tran.m, pi_mar = pi.mar, mean = mean.vec, sd = sd.vec, LL = LL, iter_LL = log.lik.iter))
+}
--- Viterbi.R
+++ Viterbi.R
@@ -4,7 +4,7 @@ Vit_hmm <- function(obs.vec, pi.mar, tran.m, mean.vec, sd.vec){
   density.m <- matrix(rep(as.double(0), Ti*N), nrow = Ti, ncol = N)
   for (i in 1:Ti){
     density.m[i,] <- mapply(
-    dnorm, 'x' = obs.vec[i], 'mean' = mean.vec, 'sd' = sd.vec,'log' = T, SIMPLIFY = T)
+      dnorm, 'x' = obs.vec[i], 'mean' = mean.vec, 'sd' = sd.vec, 'log' = T, SIMPLIFY = T)
   }
   tran.m.log <- log(tran.m)
   delta_log <- matrix(rep(NA, N*Ti), ncol = N)
@@ -12,13 +12,13 @@ Vit_hmm <- function(obs.vec, pi.mar, tran.m, mean.vec, sd.vec){
   #keep track of states
   bt <- rep(NA, Ti)
   delta_ind <- matrix(rep(NA, (Ti-1)*N), ncol = N)
-  for (i in 2:Ti){
-    delta_max <- sapply(1:N, function(x) tran.m[,x]+delta_log[i-1, x]+density.m[i,x])
-    delta_ind[i-1, ] <- apply(delta_max, 1, which.max)
-    delta_log[i,] <- apply(delta_max,1,max)
+  for (t in 2:Ti){
+    delta_max <- sapply(1:N, function(j) tran.m.log[,j]+delta_log[t-1, ]+density.m[t,j])
+    delta_ind[t-1, ] <- apply(delta_max, 2, which.max)
+    delta_log[t,] <- apply(delta_max,2,max)
   }
   bt[Ti] <- which.max(delta_log[Ti, ])
-  for (i in Ti-1:1) {
+  for (i in (Ti-1):1) {
     bt[i] <- delta_ind[i,bt[i+1]]
   }
   return(bt)
